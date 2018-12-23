c19a1b9f443832a888f126b04753fab2a71261db
segalzeyalz <segaleseo@gmail.com>
2018-12-23 11:41:47 +0200

    rendering blank component

--- src/components/Days/Days/Day.js
+++ src/components/Days/Days/Day.js
@@ -8,13 +8,25 @@ class Day extends Component {
     let {num, startDay} = this.props;
     let {MonthNum, Year} = this.props.date
     let date=moment().year(Year).month(MonthNum-1).date(num)
+    console.log(num===1 && dayNum==6)
     let dateStartDay=moment().year(startDay.year).month(startDay.month).date(startDay.day)
     let enableOrDisable = (date.isAfter(dateStartDay));
+    let dayNum = date.day()
+    let blankDays = Array.from(new Array(dayNum),(val,index)=>index+1)
+
+    if(num!==1){
     return (
       <div className={[CSS.Day, this.props.CharterDay? CSS.CharterDay:'none'].join(' ')} align="center">
         <span onClick={()=>{enableOrDisable && console.log(date.format("DD/MM/YYYY"))}} className={enableOrDisable? CSS.DayNumOn:CSS.DayNumOff}>{this.props.num}</span>
       </div>
-    );
+    );}else{ return (
+      <div className={CSS.BlankDay} align="center">
+        {blankDays.map((day,index) => (
+          <div></div>
+        ))}
+        </div>
+      )
+    }
   }
 }
 
